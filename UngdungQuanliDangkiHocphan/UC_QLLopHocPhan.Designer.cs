c69d68773f1a3fb2608721ace64abb3818aa16fe
hoangvietanh13122005-wq <hoangvietanh13122005@gmail.com>
2025-10-20 00:47:52 +0700

    check 19/10

---
+++ UngdungQuanliDangkiHocphan/UC_QLLopHocPhan.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace UngdungQuanliDangkiHocphan
+{
+    partial class UC_QLLopHocPhan
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UC_QLLopHocPhan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UC_QLLopHocPhan";
+            this.Load += new System.EventHandler(this.UC_QLLopHocPhan_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
