1d37fa43e65a741ef9b84eb7e511c67c41f3aac0
hoangvietanh13122005-wq <hoangvietanh13122005@gmail.com>
2025-10-20 00:48:54 +0700

    Revert "check 19/10"

--- UngdungQuanliDangkiHocphan/Form1.cs
+++ UngdungQuanliDangkiHocphan/Form1.cs
@@ -10,74 +10,11 @@ using System.Windows.Forms;
 
 namespace UngdungQuanliDangkiHocphan
 {
-    public partial class FormMainAdmin : Form
+    public partial class Form1 : Form
     {
-        public FormMainAdmin()
+        public Form1()
         {
             InitializeComponent();
-        }
-
-        // Hàm dùng để load UserControl vào panelMain
-        private void LoadUserControl(UserControl uc)
-        {
-            panelMain.Controls.Clear();   // Xóa control cũ trong panel
-            uc.Dock = DockStyle.Fill;     // Cho UserControl chiếm toàn bộ panel
-            panelMain.Controls.Add(uc);   // Thêm vào panelMain
-        }
-
-        private void FormMainAdmin_Load(object sender, EventArgs e)
-        {
-            // Khi mở form, hiển thị mặc định UserControl đầu tiên (nếu muốn)
-            LoadUserControl(new UC_QLHocPhan());
-        }
-
-        private void btnQLHocPhan_Click(object sender, EventArgs e)
-        {
-            LoadUserControl(new UC_QLHocPhan());
-        }
-
-        private void btnQLLopHocPhan_Click(object sender, EventArgs e)
-        {
-            LoadUserControl(new UC_QLLopHocPhan());
-        }
-
-        private void btnQLSinhVien_Click(object sender, EventArgs e)
-        {
-            LoadUserControl(new UC_QLSinhVien());
-        }
-
-        private void btnQLGiangVien_Click(object sender, EventArgs e)
-        {
-            LoadUserControl(new UC_QLGiangVien());
-        }
-
-        // Nếu có sự kiện Paint thì có thể bỏ qua nếu không dùng
-        private void panelMain_Paint(object sender, PaintEventArgs e) { }
-        private void paneMenu_Paint(object sender, PaintEventArgs e) { }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pblog_Click(object sender, EventArgs e)
-        {
-            contextMenupb.Show(pblog, new Point(0, pblog.Height));
-        }
-
-        private void panel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
