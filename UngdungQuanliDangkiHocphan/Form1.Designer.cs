679ff41d98ccb851f727b62c19519335c595b387
hoangvietanh13122005-wq <hoangvietanh13122005@gmail.com>
2025-10-22 07:58:09 +0700

    sơ bộ admin

--- UngdungQuanliDangkiHocphan/Form1.Designer.cs
+++ UngdungQuanliDangkiHocphan/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UngdungQuanliDangkiHocphan
 {
-    partial class Form1
+    partial class FormMainAdmin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.paneltop = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.panelmain = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnhocphan = new Guna.UI2.WinForms.Guna2Button();
+            this.btnlophocphan = new Guna.UI2.WinForms.Guna2Button();
+            this.btnsinhvien = new Guna.UI2.WinForms.Guna2Button();
+            this.btngiangvien = new Guna.UI2.WinForms.Guna2Button();
+            this.btnreport = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.lblten = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblma = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.paneltop.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paneltop
+            // 
+            this.paneltop.Controls.Add(this.guna2CirclePictureBox1);
+            this.paneltop.Controls.Add(this.guna2HtmlLabel1);
+            this.paneltop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paneltop.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(115)))), ((int)(((byte)(190)))));
+            this.paneltop.Location = new System.Drawing.Point(0, 0);
+            this.paneltop.Name = "paneltop";
+            this.paneltop.Size = new System.Drawing.Size(1377, 100);
+            this.paneltop.TabIndex = 0;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(115)))), ((int)(((byte)(190)))));
+            this.guna2Panel1.Controls.Add(this.btnreport);
+            this.guna2Panel1.Controls.Add(this.btngiangvien);
+            this.guna2Panel1.Controls.Add(this.btnsinhvien);
+            this.guna2Panel1.Controls.Add(this.btnlophocphan);
+            this.guna2Panel1.Controls.Add(this.btnhocphan);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 100);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(338, 419);
+            this.guna2Panel1.TabIndex = 1;
+            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BorderRadius = 15;
+            this.guna2Panel2.Controls.Add(this.lblma);
+            this.guna2Panel2.Controls.Add(this.lblten);
+            this.guna2Panel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(225)))));
+            this.guna2Panel2.Location = new System.Drawing.Point(21, 32);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(250, 100);
+            this.guna2Panel2.TabIndex = 2;
+            // 
+            // panelmain
+            // 
+            this.panelmain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelmain.Location = new System.Drawing.Point(338, 100);
+            this.panelmain.Name = "panelmain";
+            this.panelmain.Size = new System.Drawing.Size(1039, 419);
+            this.panelmain.TabIndex = 2;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(200, 30);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(904, 70);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "CPA University";
+            // 
+            // btnhocphan
+            // 
+            this.btnhocphan.BorderRadius = 15;
+            this.btnhocphan.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnhocphan.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnhocphan.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnhocphan.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnhocphan.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnhocphan.ForeColor = System.Drawing.Color.White;
+            this.btnhocphan.Location = new System.Drawing.Point(21, 138);
+            this.btnhocphan.Name = "btnhocphan";
+            this.btnhocphan.Size = new System.Drawing.Size(250, 45);
+            this.btnhocphan.TabIndex = 3;
+            this.btnhocphan.Text = "Quản lí học phần";
+            this.btnhocphan.Click += new System.EventHandler(this.btnhocphan_Click);
+            // 
+            // btnlophocphan
+            // 
+            this.btnlophocphan.BorderRadius = 15;
+            this.btnlophocphan.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnlophocphan.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnlophocphan.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnlophocphan.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnlophocphan.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnlophocphan.ForeColor = System.Drawing.Color.White;
+            this.btnlophocphan.Location = new System.Drawing.Point(21, 189);
+            this.btnlophocphan.Name = "btnlophocphan";
+            this.btnlophocphan.Size = new System.Drawing.Size(250, 45);
+            this.btnlophocphan.TabIndex = 4;
+            this.btnlophocphan.Text = "Quản lí lớp học phần";
+            this.btnlophocphan.Click += new System.EventHandler(this.btnlophocphan_Click);
+            // 
+            // btnsinhvien
+            // 
+            this.btnsinhvien.BorderRadius = 15;
+            this.btnsinhvien.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnsinhvien.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnsinhvien.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnsinhvien.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnsinhvien.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnsinhvien.ForeColor = System.Drawing.Color.White;
+            this.btnsinhvien.Location = new System.Drawing.Point(21, 240);
+            this.btnsinhvien.Name = "btnsinhvien";
+            this.btnsinhvien.Size = new System.Drawing.Size(250, 45);
+            this.btnsinhvien.TabIndex = 5;
+            this.btnsinhvien.Text = "Quản lí sinh viên";
+            this.btnsinhvien.Click += new System.EventHandler(this.btnsinhvien_Click);
+            // 
+            // btngiangvien
+            // 
+            this.btngiangvien.BorderRadius = 15;
+            this.btngiangvien.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btngiangvien.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btngiangvien.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btngiangvien.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btngiangvien.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btngiangvien.ForeColor = System.Drawing.Color.White;
+            this.btngiangvien.Location = new System.Drawing.Point(21, 291);
+            this.btngiangvien.Name = "btngiangvien";
+            this.btngiangvien.Size = new System.Drawing.Size(250, 45);
+            this.btngiangvien.TabIndex = 6;
+            this.btngiangvien.Text = "Quản lí giảng viên";
+            this.btngiangvien.Click += new System.EventHandler(this.btngiangvien_Click);
+            // 
+            // btnreport
+            // 
+            this.btnreport.BorderRadius = 15;
+            this.btnreport.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnreport.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnreport.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnreport.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnreport.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnreport.ForeColor = System.Drawing.Color.White;
+            this.btnreport.Location = new System.Drawing.Point(21, 342);
+            this.btnreport.Name = "btnreport";
+            this.btnreport.Size = new System.Drawing.Size(250, 45);
+            this.btnreport.TabIndex = 7;
+            this.btnreport.Text = "Báo cáo thống kê";
+            this.btnreport.Click += new System.EventHandler(this.btnreport_Click);
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(115)))), ((int)(((byte)(190)))));
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2CirclePictureBox1.TabIndex = 1;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // lblten
+            // 
+            this.lblten.AutoSize = false;
+            this.lblten.BackColor = System.Drawing.Color.Transparent;
+            this.lblten.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblten.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblten.Location = new System.Drawing.Point(37, 24);
+            this.lblten.Name = "lblten";
+            this.lblten.Size = new System.Drawing.Size(213, 46);
+            this.lblten.TabIndex = 0;
+            this.lblten.Text = "Tên giảng viên";
+            this.lblten.Click += new System.EventHandler(this.lblten_Click);
+            // 
+            // lblma
+            // 
+            this.lblma.AutoSize = false;
+            this.lblma.BackColor = System.Drawing.Color.Transparent;
+            this.lblma.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblma.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblma.Location = new System.Drawing.Point(37, 63);
+            this.lblma.Name = "lblma";
+            this.lblma.Size = new System.Drawing.Size(213, 37);
+            this.lblma.TabIndex = 1;
+            this.lblma.Text = "Mã giảng viên";
+            this.lblma.Click += new System.EventHandler(this.lblma_Click);
+            // 
+            // FormMainAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1377, 519);
+            this.Controls.Add(this.panelmain);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.paneltop);
+            this.Name = "FormMainAdmin";
+            this.Text = "Quản lí đăng kí học phần";
+            this.paneltop.ResumeLayout(false);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel paneltop;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button btngiangvien;
+        private Guna.UI2.WinForms.Guna2Button btnsinhvien;
+        private Guna.UI2.WinForms.Guna2Button btnlophocphan;
+        private Guna.UI2.WinForms.Guna2Button btnhocphan;
+        private Guna.UI2.WinForms.Guna2Panel panelmain;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnreport;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblma;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblten;
     }
 }
-
--- UngdungQuanliDangkiHocphan/Form1.cs
+++ UngdungQuanliDangkiHocphan/Form1.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace UngdungQuanliDangkiHocphan
 {
-    public partial class Form1 : Form
+    public partial class FormMainAdmin : Form
     {
-        public Form1()
+        public FormMainAdmin()
         {
             InitializeComponent();
+        }
+        private void LoadUserControl(UserControl uc)
+        {
+            panelmain.Controls.Clear();
+            uc.Dock = DockStyle.Fill;
+            panelmain.Controls.Add(uc);
+        }
+        private void FormMainAdmin_Load(object sender, EventArgs e)
+        {
+            LoadUserControl(new UC_QLHocPhan());
+        }
+        private void btnlophocphan_Click(object sender, EventArgs e)
+        {
+            LoadUserControl(new UC_QLLopHocPhan());
+        }
+
+        private void lblma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnhocphan_Click(object sender, EventArgs e)
+        {
+            LoadUserControl(new UC_QLHocPhan());
+        }
+
+        private void btnsinhvien_Click(object sender, EventArgs e)
+        {
+            LoadUserControl(new UC_QLSinhVien());
+        }
+
+        private void btngiangvien_Click(object sender, EventArgs e)
+        {
+            LoadUserControl(new UC_QLGiangVien());
+        }
+
+        private void btnreport_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblten_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CirclePictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
